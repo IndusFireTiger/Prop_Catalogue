0152458f8fe1631cdf8b869251076e407f6f1287
indusfiretiger <indusfiretiger@gmail.com>
2019-03-17 09:12:47 +1100

    just fetch data using axios from server without displaying it

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,27 @@
 import React, { Component } from 'react'
+import axios from "axios"
 import './App.css'
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      data: []
+    }
+  }
+
+  componentWillMount() {
+    this.getDataFromDb()
+    console.log('mount')
+  }
+
+  getDataFromDb = async () => {
+    let result = await axios("http://localhost:3001/getData")
+    console.log('result:', result)
+    this.setState({ data: result.data })
+  };
   render() {
+    console.log('state.data:',this.state.data)
     return (
       <div className="App">
           <p>
