834d281af4d99542dabdd060e94fca2eafe0f754
indusfiretiger <indusfiretiger@gmail.com>
2019-03-18 17:04:02 +1100

    add/remove functionality & some minimal css (inline and component based)

--- client/src/components/AllProperties/AllProperties.js
+++ client/src/components/AllProperties/AllProperties.js
@@ -9,6 +9,8 @@ class AllProps extends Component {
             available: [],
             saved: []
         }
+        this.addProperty = this.addProperty.bind(this);
+        this.removeProperty = this.removeProperty.bind(this);
     }
 
     componentWillMount() {
@@ -16,29 +18,55 @@ class AllProps extends Component {
     }
 
     getData = async () => {
-        let res = await axios("http://localhost:3001/getData")
+        let res = await axios("http://localhost:3001/getData") // todo: handle error
         this.setState({ available: res.data.results, saved: res.data.saved })
     };
-
+    addProperty = (e) => {
+        console.log('parent')
+        console.log('this:', this)
+        let saved = this.state.saved
+        console.log('exists:', this.state.saved)
+        console.log('target:', e.target)
+        let exists = false
+        for (let i = 0; i < saved.length; i++) {
+            if (e.target.id == saved[i].id) {
+                exists = true;
+                break
+            }
+        }
+        if (!exists) {
+            console.log('push:', this.state.available["0"])
+            saved.push(this.state.available[e.target.value])
+            this.setState({ saved })
+        } else {
+            alert('already saved')
+        }
+        console.log('saved:', this.state.saved)
+    }
+    removeProperty = (e) => {
+        let saved = this.state.saved
+        saved.splice(e.target.value, 1)
+        this.setState({ saved })
+    }
     render() {
         const { available, saved } = this.state;
         return (
             <div>
-                <div className="row" >
-                    <div className="col-sm">
+                <div className="row" style={{ display: "flex"}}>
+                    <div  className="col-sm">
                         <div className='available'>
                             <h3>Available Houses</h3>
                             {available.length <= 0
                                 ? "List Empty"
-                                : available.map((house, idx) => <House details={house} idx={idx} />)}
+                                : available.map((house, idx) => <House details={house} idx={idx} op={'Add'} opOnClick={this.addProperty}/>)}
                         </div>
                     </div>
-                    <div className="col-sm">
-                        <div className='available'>
+                    <div  className="col-sm">
+                        <div className='saved'>
                             <h3>Saved Houses</h3>
                             {saved.length <= 0
                                 ? "List Empty"
-                                : saved.map((house, idx) => <House details={house} idx={idx} />)}
+                                : saved.map((house, idx) => <House details={house} idx={idx} op={'Remove'} opOnClick={this.removeProperty}/>)}
                         </div>
                     </div>
                 </div>
--- client/src/components/House/House.js
+++ client/src/components/House/House.js
@@ -1,21 +1,28 @@
 import React, { Component } from 'react'
+import './House.css'
 
 class House extends Component {
     constructor() {
-        super()
+        super()        
+        this.opOnClick = this.opOnClick.bind(this)
     }
-    render () { 
+    opOnClick (e) {
+        console.log('child')
+        this.props.opOnClick(e)
+    }
+     
+    render() {
         let data = this.props.details
         return (
-            <div key = {this.props.idx}>
-                <div className="card house">
+            <div key={this.props.idx}>
+                <div className="card house">                
                     <div style={{ backgroundColor: data.agency.brandingColors.primary }}>
                         <img className="card-img-top brand-img" src={data.agency.logo} alt='agency'></img>
                     </div>
                     <img style={{ height: "200px" }} src={data.mainImage}></img>
                     <div className="card-body">
                         <span style={{ float: "left" }}> price: {data.price} </span>
-                        <button style={{ float: "right" }} className="btn btn-success" onClick={this.addProperty} value={this.props.idx} id={data.id}>Add</button>
+                        <button style={{ float: "right" }} className="btn btn-success" onClick={this.opOnClick} value={this.props.idx} id={data.id}>{this.props.op}</button>
                     </div>
                 </div>
             </div>
